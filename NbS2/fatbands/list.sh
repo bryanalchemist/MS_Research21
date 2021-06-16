4845d8b36596dbb513685519964d3cc464af39dc
bryanalchemist <70810408+bryanalchemist@users.noreply.github.com>
2021-06-16 17:25:56 +0800

    Update list.sh

--- NbS2/fatbands/list.sh
+++ NbS2/fatbands/list.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-Element="S"
+#This commands will list the valence electrons of element E to the file E.val.list
+Element="S" #specify the element here
 proj=*_up
+#specify at egrep as argument the valence electrons of your specified element (enclosed by quotations and separated by vertical bars).
 egrep "3S|3P" $proj >> $Element.val.list 
 
 
